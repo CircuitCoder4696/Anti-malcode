b85d8f7a5aa7c0b3ea7bfbcb135768656813ea15
CircuitCoder4696 <50537531+CircuitCoder4696@users.noreply.github.com>
2020-01-15 23:52:07 -0600

    Added source-code to the project
        Someone asked to have source-code access, so I decided to make it partially open-source.

---
+++ Pre-dev tests/anti-malcode-alpha0.0 (partial source)/opensource/data/SourceReader.d
@@ -0,0 +1,24 @@
+module opensource.data.SourceReader;
+
+private{
+    import std.file:read;
+    import std.utf:byChar;
+};
+
+private struct SourceReader_ {
+    string fp;
+    ubyte[] d;
+    //   Load indexes of specific words such as `File`, due to the potential for it being abused and|or misused.  
+};
+public class SourceReader { SourceReader_ data;
+    public void loadBinary(){ data.d= cast(ubyte[])read(data.fp.byChar); };    //   Loads the data into memory.  
+    public this(string filePath){
+        data.fp= filePath;
+        this.loadBinary();
+    };
+    public int[] _indexesOf(string substringToSeek){
+        int[] oP= new int[0];
+        //ToDo:   Write code to search for substrings, and return an array of all the indexes of each of the substrings found in a hedrogenious search.  
+        return oP;
+    };
+};
