6dfe669a96601abab518feb3115fb55bc1467acb
CircuitCoder4696 <50537531+CircuitCoder4696@users.noreply.github.com>
2020-01-22 21:24:25 -0600

    Update SourceReader.d
    I was originally playing around when I wrote this file.

--- Pre-dev tests/anti-malcode-alpha0.0 (partial source)/opensource/data/SourceReader.d
+++ Pre-dev tests/anti-malcode-alpha0.0 (partial source)/opensource/data/SourceReader.d
@@ -10,7 +10,8 @@ private struct SourceReader_ {
     ubyte[] d;
     //   Load indexes of specific words such as `File`, due to the potential for it being abused and|or misused.  
 };
-public class SourceReader { SourceReader_ data;
+public class SourceReader {
+    SourceReader_ data;
     public void loadBinary(){ data.d= cast(ubyte[])read(data.fp.byChar); };    //   Loads the data into memory.  
     public this(string filePath){
         data.fp= filePath;
